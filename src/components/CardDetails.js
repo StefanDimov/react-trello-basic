f281044bbea75b7e68905914853d67ac492c42d8
Stefan Dimov <Stefan Dimov>
2016-12-03 18:33:53 +0200

    Created a CardDetails components placeholder, can be opened in a modal

---
+++ src/components/CardDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class CardDetails extends React.Component {
+    render() {
+        return (
+            <div>
+                Card Details
+            </div>
+        )
+    }
+}
--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -1,25 +1,49 @@
 import React from 'react'
 import Board from '../components/Board'
+import CardDetails from '../components/CardDetails'
+import { Modal } from 'react-bootstrap'
 
 export default class BoardContainer extends React.Component {
     constructor(props) {
         super(props)
-        this.cardClickHandler = this.cardClickHandler.bind(this)
-        this.createCardHandler = this.createCardHandler.bind(this)
+        this.state = {
+            showCardDetails: false
+        }
     }
+
+    closeModal(modalKey) {
+        this.setState({
+            [modalKey]: false
+        })
+    }
+
     cardClickHandler() {
-        console.log('card clicked');
+        this.setState({
+            showCardDetails: true
+        })
     }
+
     createCardHandler() {
         console.log('add card clicked')
     }
+
     render() {
         const { boardData } = this.props
 
-        return <Board
+        return (
+            <div>
+                <Board
                     title={boardData.title}
                     lists={boardData.lists}
-                    onCardClick={this.cardClickHandler}
-                    onCreateCardClick={this.createCardHandler} />
+                    onCardClick={this.cardClickHandler.bind(this)}
+                    onCreateCardClick={this.createCardHandler.bind(this)} />
+
+                <Modal show={this.state.showCardDetails} onHide={this.closeModal.bind(this, 'showCardDetails')}>
+                    <Modal.Body>
+                        <CardDetails></CardDetails>
+                    </Modal.Body>
+                </Modal>
+            </div>
+        )
     }
 }
