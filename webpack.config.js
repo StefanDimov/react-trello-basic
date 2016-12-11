a122f9811d7def85e0c0b42555ad293f2400d727
Stefan Dimov <Stefan Dimov>
2016-12-11 09:37:44 +0200

    Added  command to npm scripts - optimizes bundle and writes to disk;

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,72 @@
 var path = require('path')
+var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-    devtool: 'inline-source-map',
-    entry: path.join(__dirname, 'src', 'app.js'),
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.png$/, loader: 'url-loader?limit=100000' },
-            { test: /\.jpg$/, loader: 'file-loader' },
-            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'build'),
-        inline: true,
-        noInfo: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'index.html'),
-            inject: 'body'
-        })
+var TARGET = process.env.npm_lifecycle_event
+var isProduction = TARGET === 'build'
+
+var config = {}
+
+//  General
+// ---------------------------------------------
+
+config.devtool = isProduction ? null : 'eval'
+
+config.devServer = {
+    contentBase: path.join(__dirname, 'build'),
+    inline: true,
+    stats: {
+        chunks: false
+    }
+}
+
+// Entry / Output
+// ---------------------------------------------
+
+config.entry = {
+    'app': path.join(__dirname, 'src', 'app.js')
+}
+
+config.output = {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].bundle.js'
+}
+
+//  Loaders
+// ---------------------------------------------
+
+config.module = {
+    loaders: [
+        { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
+        { test: /\.css$/, loader: 'style-loader!css-loader' },
+        { test: /\.png$/, loader: 'url-loader?limit=100000' },
+        { test: /\.jpg$/, loader: 'file-loader' },
+        { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
     ]
 }
+
+//  Plugins
+// ---------------------------------------------
+
+config.plugins = []
+
+config.plugins.push(new HtmlWebpackPlugin({
+    template: path.join(__dirname, 'src', 'index.html'),
+    inject: 'body'
+}))
+
+if (isProduction) {
+    config.plugins.push(
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+               warnings: false
+            }
+        })
+    )
+}
+
+module.exports = config;
