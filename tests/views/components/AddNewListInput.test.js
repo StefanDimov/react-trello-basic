40b4f0828f6bc4a57fb7cb5abd1c0b058f047877
Stefan Dimov <Stefan Dimov>
2016-12-24 18:58:52 +0200

    AddNewListInput Tests: refactored

--- tests/views/components/AddNewListInput.test.js
+++ tests/views/components/AddNewListInput.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import renderer from 'react-test-renderer'
 import { shallow } from 'enzyme'
 
 import AddNewListInput from '../../../src/views/components/AddNewListInput'
@@ -15,49 +16,33 @@ describe('AddNewListInput', () => {
         onCreateList.mockClear()
     })
 
-    describe('container', () => {
-        it('should be a form element', () => {
-            const wrapper = shallow(<AddNewListInput onCreateList={onCreateList} />)
-            expect(wrapper.is('form')).toBe(true)
-        })
+    it('should render correctly', () => {
+        const tree = renderer.create(
+            <AddNewListInput onCreateList={onCreateList} />
+        ).toJSON()
 
-        it('should have proper custom styles', () => {
-            const wrapper = shallow(<AddNewListInput onCreateList={onCreateList} />)
-            expect(wrapper.prop('className')).toBe('my-list-container-item')
-        })
-
-        it('should call onCreateList with the new list\'s name on submit', () => {
-            const wrapper = shallow(<AddNewListInput onCreateList={onCreateList} />)
-
-            const eventMock = { preventDefault: jest.fn() }
-
-            wrapper.simulate('submit', eventMock)
-            expect(onCreateList.mock.calls[0][0]).toBe('')
-
-            wrapper.find(FormControl).simulate('change', inputChangeEventDataWithName)
-            wrapper.simulate('submit', eventMock)
-            expect(onCreateList.mock.calls[1][0]).toBe(inputChangeEventDataWithName.target.value)
-        })
+        expect(tree).toMatchSnapshot();
     })
 
-    describe('input', () => {
-        it('should be represented by a FormControl', () => {
-            const wrapper = shallow(<AddNewListInput onCreateList={onCreateList} />)
-            expect(wrapper.find(FormControl).length).toBe(1)
-        })
+    it('should call onCreateList with the new list\'s name on submit', () => {
+        const wrapper = shallow(<AddNewListInput onCreateList={onCreateList} />)
 
-        it('should be wrapped by a FormGroup', () => {
-            const wrapper = shallow(<AddNewListInput onCreateList={onCreateList} />)
-            expect(wrapper.find(FormControl).parent().is(FormGroup)).toBe(true)
-        })
+        const eventMock = { preventDefault: jest.fn() }
 
-        it('should update state on change', () => {
-            const wrapper = shallow(<AddNewListInput onCreateList={onCreateList} />)
+        wrapper.simulate('submit', eventMock)
+        expect(onCreateList.mock.calls[0][0]).toBe('')
 
-            expect(wrapper.find(FormControl).prop('value')).toBe('')
+        wrapper.find(FormControl).simulate('change', inputChangeEventDataWithName)
+        wrapper.simulate('submit', eventMock)
+        expect(onCreateList.mock.calls[1][0]).toBe(inputChangeEventDataWithName.target.value)
+    })
 
-            wrapper.find(FormControl).simulate('change', inputChangeEventDataWithName)
-            expect(wrapper.find(FormControl).prop('value')).toBe(inputChangeEventDataWithName.target.value)
-        })
+    it('should update state on change', () => {
+        const wrapper = shallow(<AddNewListInput onCreateList={onCreateList} />)
+
+        expect(wrapper.find(FormControl).prop('value')).toBe('')
+
+        wrapper.find(FormControl).simulate('change', inputChangeEventDataWithName)
+        expect(wrapper.find(FormControl).prop('value')).toBe(inputChangeEventDataWithName.target.value)
     })
 })
