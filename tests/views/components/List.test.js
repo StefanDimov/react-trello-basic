d58d557aa02f9a0888ce2e70e08c445ff1e3b091
Stefan Dimov <Stefan Dimov>
2016-12-25 09:30:33 +0200

    List Tests: refactored

--- tests/views/components/List.test.js
+++ tests/views/components/List.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import renderer from 'react-test-renderer'
 import { shallow } from 'enzyme'
 
 import List from '../../../src/views/components/List'
@@ -9,73 +10,64 @@ describe('List', () => {
 
     let list
     let onCardClick = jest.fn()
+    const cardWithId = { listId: '1', id: 'uniqueId' }
+    const anotherCardWithId = { listId: '2', id: 'uniqueId2' }
 
     beforeEach(() => {
         list = { id: 'uniqueId', cards: [] }
         onCardClick.mockClear()
     })
 
-    describe('container', () => {
+    describe('rendering', () => {
 
-        it('should be bootstrap panel', () => {
-            const wrapper = shallow(<List list={list} onCardClick={onCardClick} />)
-            expect(wrapper.is(Panel)).toBe(true)
-        })
-
-        it('should have title set on bootstrap panel', () => {
+        it('should render correctly with title', () => {
             list.title = 'title'
-            const wrapper = shallow(<List list={list} onCardClick={onCardClick} />)
-            expect(wrapper.prop('header')).toBe(list.title)
+            const tree = renderer.create(
+                <List list={list} onCardClick={onCardClick} />
+            ).toJSON()
+
+            expect(tree).toMatchSnapshot()
         })
 
-        it('should have proper custom styles', () => {
-            const wrapper = shallow(<List list={list} onCardClick={onCardClick} />)
-            expect(wrapper.prop('className')).toBe('my-list-container-item')
-        })
-
-        it('should render children', () => {
-            const wrapper = shallow(
+        it('should render correctly with children', () => {
+            const tree = renderer.create(
                 <List list={list} onCardClick={onCardClick}>
                     <span>unique</span>
                 </List>
-            )
-            expect(wrapper.containsMatchingElement(<span>unique</span>)).toBe(true)
+            ).toJSON()
+
+            expect(tree).toMatchSnapshot()
+        })
+
+        it('should render correctly with a card', () => {
+            list.cards = [cardWithId]
+            const tree = renderer.create(
+                <List list={list} onCardClick={onCardClick} />
+            ).toJSON()
+
+            expect(tree).toMatchSnapshot()
+        })
+
+        it('should render correctly with two card', () => {
+            list.cards = [cardWithId, anotherCardWithId]
+            const tree = renderer.create(
+                <List list={list} onCardClick={onCardClick} />
+            ).toJSON()
+
+            expect(tree).toMatchSnapshot()
+        })
+
+        it('should render correctly with no cards', () => {
+            list.cards = []
+            const tree = renderer.create(
+                <List list={list} onCardClick={onCardClick} />
+            ).toJSON()
+
+            expect(tree).toMatchSnapshot()
         })
     })
 
     describe('cards', () => {
-
-        const cardWithId = { listId: '1', id: 'uniqueId' }
-        const anotherCardWithId = { listId: '2', id: 'uniqueId2' }
-
-        it('should have a ListGroup container', () => {
-            list.cards = [cardWithId]
-            const wrapper = shallow(<List list={list} onCardClick={onCardClick} />)
-            expect(wrapper.children(ListGroup).length).toBe(1)
-            expect(wrapper.children(ListGroup).find(Card).length).toBe(1)
-        })
-
-        it('should not have a rendered ListGroup container if there are no cards', () => {
-            list.cards = []
-            const wrapper = shallow(<List list={list} onCardClick={onCardClick} />)
-            expect(wrapper.children(ListGroup).length).toBe(0)
-            expect(wrapper.find(Card).length).toBe(0)
-        })
-
-        it('should each be wrapped in a ListGroupItem container', () => {
-            list.cards = [cardWithId]
-            const wrapper = shallow(<List list={list} onCardClick={onCardClick} />)
-            expect(wrapper.children(ListGroup).children(ListGroupItem).length).toBe(1)
-            expect(wrapper.children(ListGroup).children(ListGroupItem).children(Card).length).toBe(1)
-        })
-
-        it('should each have their ids set on ListGroupItem as a key prop', () => {
-            list.cards = [cardWithId, anotherCardWithId]
-            const wrapper = shallow(<List list={list} onCardClick={onCardClick} />)
-
-            expect(wrapper.find(ListGroupItem).first().key()).toBe(cardWithId.id)
-            expect(wrapper.find(ListGroupItem).last().key()).toBe(anotherCardWithId.id)
-        })
 
         it('should each be passed to the onCardClick func on ListGroupItem click', () => {
             list.cards = [cardWithId, anotherCardWithId]
@@ -86,14 +78,6 @@ describe('List', () => {
 
             wrapper.find(ListGroupItem).last().simulate('click')
             expect(onCardClick.mock.calls[1][0]).toBe(anotherCardWithId)
-        })
-
-        it('should each be passed the card data to it', () => {
-            list.cards = [cardWithId, anotherCardWithId]
-            const wrapper = shallow(<List list={list} onCardClick={onCardClick} />)
-
-            expect(wrapper.find(Card).first().prop('card')).toBe(cardWithId)
-            expect(wrapper.find(Card).last().prop('card')).toBe(anotherCardWithId)
         })
     })
 })
